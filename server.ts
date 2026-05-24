1901b32948eff9a1b3dce0e825e3347001b3cf9e
Sridhr <sridharkonda553@gmail.com>
2026-05-24 18:50:19 +0530

    Update server PORT to bind to process.env.PORT for Render support

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import { createServer as createViteServer } from 'vite';
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   
   const server = http.createServer(app);
   
